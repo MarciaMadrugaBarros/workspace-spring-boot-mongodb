7bf0323f7f73c860caa824b867a1d77c2042a246
MarciaMadrugaBarros <marcinha_madruga@hotmail.com>
2023-04-24 12:21:10 -0300

    Inserção de usuário com POST

--- src/main/java/com/marciamadrugab/workshopmongo/services/UserService.java
+++ src/main/java/com/marciamadrugab/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.marciamadrugab.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marciamadrugab.workshopmongo.domain.User;
+import com.marciamadrugab.workshopmongo.dto.UserDTO;
 import com.marciamadrugab.workshopmongo.repository.UserRepository;
 import com.marciamadrugab.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -20,10 +22,15 @@ public class UserService {
 	}
 	
 	public User findById(String id) {
-		User user = repo.findOne(id);
-		if(user == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado");
-		}
-		return user;
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
