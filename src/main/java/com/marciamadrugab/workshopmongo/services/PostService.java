6c6c7d16a55db1d9b9391ed631730f01bd2c2ed8
MarciaMadrugaBarros <marcinha_madruga@hotmail.com>
2023-04-24 21:09:49 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marciamadrugab/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.marciamadrugab.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marciamadrugab.workshopmongo.domain.Post;
+import com.marciamadrugab.workshopmongo.repository.PostRepository;
+import com.marciamadrugab.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
