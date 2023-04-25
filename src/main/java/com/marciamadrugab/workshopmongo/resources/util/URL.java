c59cc91689e8ac258ed570dee709f9955bf35a62
MarciaMadrugaBarros <marcinha_madruga@hotmail.com>
2023-04-24 22:39:03 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/marciamadrugab/workshopmongo/resources/util/URL.java
@@ -0,0 +1,15 @@
+package com.marciamadrugab.workshopmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+
+	public static String decodePram(String text) {
+		try {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return"";
+		}
+	}
+}
